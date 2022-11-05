04109c85df28a1346c1b2c8e0354dbba414c19b8
Alberto Valenzuela <avalen28@gmail.com>
2022-11-05 12:34:48 +0100

    text errors fixed. lab done

--- js/index.js
+++ js/index.js
@@ -1,10 +1,10 @@
 // Iteration 1: Names and Input
 
 let hacker1 = "Gerard";
-console.log(The driver's name is ${hacker1});
+console.log(`The driver's name is ${hacker1}`);
 
 let hacker2 = "Alberto";
-console.log(The navigator's name is ${hacker2});
+console.log(`The navigator's name is ${hacker2}`);
     
 // Iteration 2: Conditionals
 
@@ -12,11 +12,11 @@ let hacker1Length = hacker1.length;
 let hacker2Length = hacker2.length;
 
 if (hacker1Length > hacker2Length) {
-    console.log(The driver has the longest name, it has ${hacker1Length} characters.)
+    console.log(`The driver has the longest name, it has ${hacker1Length} characters.`)
 } else if (hacker1Length === hacker2Length) {
-    console.log(Wow, you both have equally long names, ${hacker1Length} characters!)
+    console.log(`Wow, you both have equally long names, ${hacker1Length} characters!`)
 } else {
-    console.log(It seems that the navigator has the longest name, it has ${hacker2Length} characters.)
+    console.log(`It seems that the navigator has the longest name, it has ${hacker2Length} characters.`)
 };
 
 // Iteration 3: Loops
